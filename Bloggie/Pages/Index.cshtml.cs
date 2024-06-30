0be33151338b3b801137cab0e7a3ad18d144ad3f
maharshi111 <97938886+maharshi111@users.noreply.github.com>
2024-06-30 15:06:11 +0530

    done vedio 44 (sec 6)

--- Bloggie/Pages/Index.cshtml.cs
+++ Bloggie/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Models.Domain;
+using Bloggie.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Bloggie.Pages
@@ -6,15 +8,26 @@ namespace Bloggie.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IBlogPostRepository blogPostRepository;
+         public List<BlogPost> Blogs { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,IBlogPostRepository blogPostRepository)
         {
             _logger = logger;
+            this.blogPostRepository = blogPostRepository;
         }
 
-        public void OnGet()
+        //public async Task OnGet()
+        //{
+        //    var blogs  = await blogPostRepository.GetAllAsync();
+        //    Blogs = blogs.ToList();
+        //}
+        public async Task<IActionResult> OnGet()
         {
-
+            var blogs = (await blogPostRepository.GetAllAsync());
+            Blogs = blogs.ToList();
+            return Page();
         }
+        //you can write the method OnGet in both ways see Q2 IN OtherKeyConcepts e notes also see Q1 in that to known why we can't  assign the var blogs outside OnGET
     }
 }
